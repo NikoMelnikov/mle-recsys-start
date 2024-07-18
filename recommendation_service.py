4f55d6f60fe617b0dc381e1d358328620c45f81c
Nikolay Melnikov <mle-user@epdigp5m9elt65vutfog.auto.internal>
2024-07-18 13:32:07 +0000

    service with responce

--- recommendation_service.py
+++ recommendation_service.py
@@ -5,6 +5,8 @@ from contextlib import asynccontextmanager
 
 logger = logging.getLogger("uvicorn.error")
 
+app = FastAPI()
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # код ниже (до yield) выполнится только один раз при запуске сервиса
@@ -12,9 +14,69 @@ async def lifespan(app: FastAPI):
     yield
     # этот код выполнится только один раз при остановке сервиса
     logger.info("Stopping")
-    
+import logging as logger
+import pandas as pd
+
+class Recommendations:
+
+    def __init__(self):
+
+        self._recs = {"personal": None, "default": None}
+        self._stats = {
+            "request_personal_count": 0,
+            "request_default_count": 0,
+        }
+
+    def load(self, type, path, **kwargs):
+        """
+        Загружает рекомендации из файла
+        """
+
+        logger.info(f"Loading recommendations, type: {type}")
+        self._recs[type] = pd.read_parquet(path, **kwargs)
+        if type == "personal":
+            self._recs[type] = self._recs[type].set_index("user_id")
+        logger.info(f"Loaded")
+
+    def get(self, user_id: int, k: int=100):
+        """
+        Возвращает список рекомендаций для пользователя
+        """
+        try:
+            recs = self._recs["personal"].loc[user_id]
+            recs = recs["item_id"].to_list()[:k]
+            self._stats["request_personal_count"] += 1
+        except KeyError:
+            recs = self._recs["default"]
+            recs = recs["item_id"].to_list()[:k]
+            self._stats["request_default_count"] += 1
+        except:
+            logger.error("No recommendations found")
+            recs = []
+
+        return recs
+
+    def stats(self):
+
+        logger.info("Stats for recommendations")
+        for name, value in self._stats.items():
+            logger.info(f"{name:<30} {value} ")    
 # создаём приложение FastAPI
 app = FastAPI(title="recommendations", lifespan=lifespan)
+
+rec_store = Recommendations()
+
+rec_store.load(
+    "personal",
+    "/home/mle-user/mle_projects/mle-recsys-start/final_recommendations_feat.parquet",
+    columns=["user_id", "item_id", "rank"],
+)
+rec_store.load(
+    "default",
+    "/home/mle-user/mle_projects/mle-recsys-start/top_recs.parquet",
+    columns=["item_id", "rank"],
+)
+
 
 @app.post("/recommendations")
 async def recommendations(user_id: int, k: int = 100):
@@ -22,6 +84,6 @@ async def recommendations(user_id: int, k: int = 100):
     Возвращает список рекомендаций длиной k для пользователя user_id
     """
 
-    recs = []
+    recs = rec_store.get(user_id, k)
 
     return {"recs": recs}
